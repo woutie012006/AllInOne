012b5aa1548426b1bdb65fcc6f88904a2ff6592c
Wouter Vanmulken <woutie012006@hotmail.nl>
2015-03-30 21:52:13 +0100

    May have deleted the previous repo because i didn't really want to do anything with it further. But now well.... i do.
    Here is the code i had so far.

---
+++ All In One/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace All_In_One
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnListChanger1 = new System.Windows.Forms.Button();
+            this.txtBoxListChanger2 = new System.Windows.Forms.RichTextBox();
+            this.txtBoxListChanger1 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnFileChanger1 = new System.Windows.Forms.Button();
+            this.txtBoxFileChanger3 = new System.Windows.Forms.TextBox();
+            this.txtBoxFileChanger2 = new System.Windows.Forms.TextBox();
+            this.txtBoxFileChanger1 = new System.Windows.Forms.TextBox();
+            this.openFolder1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFolder2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.Switcher = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.Switcher);
+            this.tabControl1.Location = new System.Drawing.Point(-3, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(357, 297);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.btnListChanger1);
+            this.tabPage1.Controls.Add(this.txtBoxListChanger2);
+            this.tabPage1.Controls.Add(this.txtBoxListChanger1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(349, 271);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "List Changer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tag you wish to add";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = ">";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "<";
+            // 
+            // btnListChanger1
+            // 
+            this.btnListChanger1.Location = new System.Drawing.Point(180, 6);
+            this.btnListChanger1.Name = "btnListChanger1";
+            this.btnListChanger1.Size = new System.Drawing.Size(150, 50);
+            this.btnListChanger1.TabIndex = 2;
+            this.btnListChanger1.Text = "Add tag";
+            this.btnListChanger1.UseVisualStyleBackColor = true;
+            this.btnListChanger1.Click += new System.EventHandler(this.buttonGo1_Click);
+            // 
+            // txtBoxListChanger2
+            // 
+            this.txtBoxListChanger2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBoxListChanger2.Location = new System.Drawing.Point(10, 67);
+            this.txtBoxListChanger2.Name = "txtBoxListChanger2";
+            this.txtBoxListChanger2.Size = new System.Drawing.Size(330, 193);
+            this.txtBoxListChanger2.TabIndex = 1;
+            this.txtBoxListChanger2.Text = "";
+            // 
+            // txtBoxListChanger1
+            // 
+            this.txtBoxListChanger1.Location = new System.Drawing.Point(26, 35);
+            this.txtBoxListChanger1.Name = "txtBoxListChanger1";
+            this.txtBoxListChanger1.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxListChanger1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.btnFileChanger1);
+            this.tabPage2.Controls.Add(this.txtBoxFileChanger3);
+            this.tabPage2.Controls.Add(this.txtBoxFileChanger2);
+            this.tabPage2.Controls.Add(this.txtBoxFileChanger1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(349, 271);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "File Changer";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(126, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "What to searsch for";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(126, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Move to this folder";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(126, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Folder to searsch";
+            // 
+            // btnFileChanger1
+            // 
+            this.btnFileChanger1.Location = new System.Drawing.Point(129, 219);
+            this.btnFileChanger1.Name = "btnFileChanger1";
+            this.btnFileChanger1.Size = new System.Drawing.Size(75, 23);
+            this.btnFileChanger1.TabIndex = 13;
+            this.btnFileChanger1.Text = "Go";
+            this.btnFileChanger1.UseVisualStyleBackColor = true;
+            this.btnFileChanger1.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // txtBoxFileChanger3
+            // 
+            this.txtBoxFileChanger3.Location = new System.Drawing.Point(86, 175);
+            this.txtBoxFileChanger3.Name = "txtBoxFileChanger3";
+            this.txtBoxFileChanger3.Size = new System.Drawing.Size(183, 20);
+            this.txtBoxFileChanger3.TabIndex = 12;
+            // 
+            // txtBoxFileChanger2
+            // 
+            this.txtBoxFileChanger2.Location = new System.Drawing.Point(86, 108);
+            this.txtBoxFileChanger2.Name = "txtBoxFileChanger2";
+            this.txtBoxFileChanger2.Size = new System.Drawing.Size(183, 20);
+            this.txtBoxFileChanger2.TabIndex = 11;
+            this.txtBoxFileChanger2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox4_MouseDown);
+            // 
+            // txtBoxFileChanger1
+            // 
+            this.txtBoxFileChanger1.BackColor = System.Drawing.SystemColors.Window;
+            this.txtBoxFileChanger1.Location = new System.Drawing.Point(86, 48);
+            this.txtBoxFileChanger1.Name = "txtBoxFileChanger1";
+            this.txtBoxFileChanger1.Size = new System.Drawing.Size(183, 20);
+            this.txtBoxFileChanger1.TabIndex = 10;
+            this.txtBoxFileChanger1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox3_MouseDown);
+            // 
+            // openFolder1
+            // 
+            this.openFolder1.SelectedPath = "C:\\Users\\%username%\\Desktop";
+            // 
+            // openFolder2
+            // 
+            this.openFolder2.SelectedPath = "C:\\Users\\%username%\\Desktop";
+            // 
+            // Switcher
+            // 
+            this.Switcher.Location = new System.Drawing.Point(4, 22);
+            this.Switcher.Name = "Switcher";
+            this.Switcher.Padding = new System.Windows.Forms.Padding(3);
+            this.Switcher.Size = new System.Drawing.Size(349, 271);
+            this.Switcher.TabIndex = 2;
+            this.Switcher.Text = "Switcher";
+            this.Switcher.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(346, 288);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "All-in-One";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnListChanger1;
+        private System.Windows.Forms.RichTextBox txtBoxListChanger2;
+        private System.Windows.Forms.TextBox txtBoxListChanger1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnFileChanger1;
+        private System.Windows.Forms.TextBox txtBoxFileChanger3;
+        private System.Windows.Forms.TextBox txtBoxFileChanger2;
+        private System.Windows.Forms.TextBox txtBoxFileChanger1;
+        private System.Windows.Forms.FolderBrowserDialog openFolder1;
+        private System.Windows.Forms.FolderBrowserDialog openFolder2;
+        private System.Windows.Forms.TabPage Switcher;
+    }
+}
+
---
+++ All In One/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace All_In_One
+{
+    public partial class Form1 : Form
+    {
+        string dirFromFC;// fc stands for file changer
+        string dirToFC;
+        string searchFC;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //<summary>
+        //Button to add the tags to the lines in the list
+        //</summary
+        private void buttonGo1_Click(object sender, EventArgs e)
+        {
+            string tag = txtBoxListChanger1.Text;
+            String s=String.Empty;
+            String Output="";
+
+            if (txtBoxListChanger1.Text == "" || txtBoxListChanger2.Text == "")
+            {
+                MessageBox.Show("Please make sure you filled both textboxes","Message");
+            }
+            else
+            {
+              for (int i = 0; i < txtBoxListChanger2.Lines.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        s = txtBoxListChanger2.Lines[i];
+                        Output += "<" + tag + ">" + txtBoxListChanger2.Lines[i] + "</" + tag + ">";
+                    }
+                    else if (txtBoxListChanger2.Lines[i] == "")
+                    {}
+                    else
+                    {
+                        s = txtBoxListChanger2.Lines[i];
+                        Output += "\n" + "<" + tag + ">" + txtBoxListChanger2.Lines[i] + "</" + tag + ">";
+                    }
+
+                }
+                txtBoxListChanger2.Text = Output;
+            }       
+        }
+
+        //<summary>
+        //Button to change the location of files
+        //</summary>
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            if (txtBoxListChanger1.Text == "" || txtBoxListChanger2.Text == "" || txtBoxFileChanger1.Text == "")
+            {
+                MessageBox.Show("you didn\'t give a value to one of the textboxes ", "Mkay", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                txtBoxFileChanger1.Text = dirFromFC;
+                txtBoxFileChanger2.Text = dirToFC;
+                searchFC = txtBoxFileChanger3.Text;
+
+
+                string[] dirs = System.IO.Directory.GetFiles(dirFromFC, searchFC + "*");
+                if (dirs.Length == 0)
+                {
+                    MessageBox.Show("no files found in " + dirFromFC, "message");
+                }
+                else
+                {
+                    foreach (string dir in dirs)
+                    {
+
+                        string temporary = dir.Remove(0, dirFromFC.Length);
+                        System.IO.File.Move(dir, dirToFC + temporary);
+
+                    }
+                    MessageBox.Show("all files that had " + searchFC + " in their name have been moved to " + dirToFC, "message");
+                }
+            }
+        }
+
+        //<summary>
+        //Shows the file explorer to open a destination in which the files will be searched,
+        //the standard destination is set to the desktop.
+        //</summary>
+        private void textBox3_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (openFolder1.ShowDialog() == DialogResult.OK)
+            {
+                txtBoxFileChanger1.Text = openFolder1.SelectedPath;
+                dirFromFC = openFolder1.SelectedPath;
+            }
+        }
+
+        //<summary>
+        //Shows the file explorer to open a destination in which the found files will be set,
+        //the standard destination is set to the desktop.
+        //</summary>
+        private void textBox4_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (openFolder2.ShowDialog() == DialogResult.OK)
+            {
+                txtBoxFileChanger2.Text = openFolder2.SelectedPath;
+                dirToFC = openFolder2.SelectedPath;
+            }
+        }
+    }
+}
